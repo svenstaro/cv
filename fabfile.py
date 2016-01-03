aebdc8b735bd9f8dfe1a67aa3b1c980a0d1e2165
Sven-Hendrik Haase <sh@lutzhaase.com>
2016-01-03 13:28:28 +0100

    Multiple deploy dirs

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ env.colorize_errors = True
 
 def deploy():
     code_dir = "/home/svenstaro/prj/cv"
-    deploy_dir = "/srv/http/pub/"
+    deploy_dirs = ["/srv/http/pub/customalized.org", "/srv/http/pub/pseudoform.org"]
 
     # clone/pull using keybearer first
     with settings(warn_only=True):
@@ -16,4 +16,5 @@ def deploy():
             run("git clone git@github.com:svenstaro/cv.git %s" % code_dir)
     with cd(code_dir):
         run("git pull")
-        sudo("cp index.html cv.css %s" % deploy_dir)
+        for deploy_dir in deploy_dirs:
+            sudo("cp index.html cv.css %s" % deploy_dir)
